b293d429a4b0407e3fd987beabbd219e0013422c
mukul.dani <mukul1206>
2018-10-13 22:15:51 +0530

    LoginRoom error resolved

--- LoginRoom/app/src/main/java/room/login/com/loginroom/MainActivity.java
+++ LoginRoom/app/src/main/java/room/login/com/loginroom/MainActivity.java
@@ -1,8 +1,11 @@
 package room.login.com.loginroom;
 
 import android.arch.persistence.room.Room;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private Button signupButton;
     private Button loginButton;
     private UserDataBase userDataBase;
+    private SharedPreferences sharedPreferences;
+    private final static String PREF_CREDENTIALS = "Credentials";
+    private final static String PREF_KEY_USERNAME = "Username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         signupButton = findViewById(R.id.signupButton);
         loginButton = findViewById(R.id.loginButton);
-
+        sharedPreferences = getSharedPreferences(PREF_CREDENTIALS, Context.MODE_PRIVATE);
+        if(isAlreadySignedup()){
+            signupButton.setEnabled(false);
+            loginButton.setEnabled(true);
+        }
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 users.setUsername(usernameEditText.getText().toString());
                 users.setPassword(passwordEditText.getText().toString());
                 userDataBase.usersDao().insertUserDetails(users);
+                sharedPreferences.edit().putString(PREF_KEY_USERNAME,usernameEditText.getText().toString()).apply();
                 Toast.makeText(MainActivity.this,"user Details saved", Toast.LENGTH_LONG).show();
 
             }
@@ -43,8 +54,21 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Users users
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                boolean checkUserDetails = userDataBase.usersDao().checkLogin(username,password);
+                if(checkUserDetails){
+                    Toast.makeText(MainActivity.this,"Welcome",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this,WelcomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"Please Check the credentials",Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    public boolean isAlreadySignedup() {
+        return sharedPreferences.contains(PREF_KEY_USERNAME);
     }
 }
